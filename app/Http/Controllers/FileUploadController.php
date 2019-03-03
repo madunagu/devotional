d00c959739b0b6fa01e9a923806fe5817d720681
= <ekenemadunagu@gmail.com>
2019-03-03 23:43:39 +0100

    image upload controller

---
+++ app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+    public function fileUpload(Request $request)
+    {
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:12048',
+        ]);
+        $image = $request->file('image');
+
+        $input['imagename'] = time().'.'.$image->getClientOriginalExtension();
+
+        $destinationPath = public_path('/images');
+
+        $image->move($destinationPath, $input['imagename']);
+
+        $data = $input['imagename'];
+
+        return response()->json(compact('data'),201);
+    }
+}
