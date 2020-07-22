1cab5de085060b8d6fafa6b3a8219e3aa2759dcf
Ekene Madunagu <ekenemadunagu@gmail.com>
2020-07-22 22:44:38 +0100

    added user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+      $this->call(UserSeeder::class);
       $this->call(AddressSeeder::class);
       $this->call(ChurchSeeder::class);
       $this->call(EventSeeder::class);
