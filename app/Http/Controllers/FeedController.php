2c3551f2c5549e1fd864031d92aadf505571da92
Ekene Madunagu <ekenemadunagu@gmail.com>
2021-02-18 19:51:42 +0100

    added liked to feed

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -9,6 +9,7 @@ use App\Http\Resources\FeedCollection;
 use App\User;
 use App\VideoPost;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
@@ -22,28 +23,50 @@ class FeedController extends Controller
             return response()->json('invalid feed type', 422);
         }
         $user = Auth::user();
+        $userId = $user->id;
         $following = $user->following()->pluck('user_id');
 
         $feeds = Feed::with([
-            'parentable' => function (MorphTo $morphTo) {
+            'parentable' => function (MorphTo $morphTo) use ($userId) {
                 $morphTo->morphWithCount([
-                    AudioPost::class => ['comments', 'likes', 'views'],
-                    VideoPost::class => ['comments', 'likes', 'views'],
-                    Post::class => ['comments', 'likes', 'views'],
-                    Event::class => ['comments', 'attendees', 'views'],
+                    AudioPost::class => [
+                        'comments', 'likes', 'views',
+                        'likes as liked' => function (Builder $query) use ($userId) {
+                            $query->where('user_id', $userId);
+                        },
+                    ],
+                    VideoPost::class => [
+                        'comments', 'likes', 'views',
+                        'likes as liked' => function (Builder $query) use ($userId) {
+                            $query->where('user_id', $userId);
+                        },
+                    ],
+                    Post::class => [
+                        'comments', 'likes', 'views',
+                        'likes as liked' => function (Builder $query) use ($userId) {
+                            $query->where('user_id', $userId);
+                        },
+                    ],
+                    Event::class => [
+                        'comments', 'attendees',
+                        'attendees as attending' => function (Builder $query) use ($userId) {
+                            $query->where('user_id', $userId);
+                        },
+                        'views'
+                    ],
                 ]);
 
                 $morphTo->morphWith([
                     AudioPost::class => ['user'],
                     VideoPost::class => ['user'],
                     Post::class => ['user'],
-                    Event::class => ['poster','user'],
+                    Event::class => ['poster', 'user'],
                 ]);
             }
         ])
-        
-        ->whereIn('postable_id', $following)
-        ->orderBy('created_at', 'desc');
+
+            ->whereIn('postable_id', $following)
+            ->orderBy('created_at', 'desc');
         if (!empty($type)) {
             $feeds = $feeds->where('parentable_type', $type);
         }
